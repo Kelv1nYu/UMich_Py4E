97b43ec6600294d7e7aefd89298c2163054c893d
KelvinYu <ykq1312@126.com>
2017-12-16 22:04:20 +0800

    Create ex_03_02.py

---
+++ ex_03_02.py
@@ -0,0 +1,14 @@
+hrs = input("Enter Hours:")
+rate = input("Enter Rate:")
+try:
+    h = float(hrs)
+    r = float(rate)
+except:
+    print("Error, pleas input a number!")
+    quit()
+
+if h > 40:
+    pay = 40 * r + (h - 40) * 1.5 * r
+else :
+    pay = h * r
+print(pay)
