7d8da8fa8e9342e80a554b3e4d2d8d4d817b6168
KelvinYu <ykq1312@126.com>
2017-12-18 00:04:38 +0800

    Create ex_05_01.py

---
+++ ex_05_01.py
@@ -0,0 +1,16 @@
+num = 0
+tot = 0.0
+while True:
+    sval = input("Enter a number")
+    if sval == 'done':
+        break
+    try:
+        fval = folat(sval)
+    except:
+        print("Invalid input")
+        continue
+    print(fval)
+    num = num + 1
+    tot = tot + fval
+
+print(tot, num, tot / num)
