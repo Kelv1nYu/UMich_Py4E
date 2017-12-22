6ec4d8be95a9e08b48d7566826d69bdaefd6e383
KelvinYu <ykq1312@126.com>
2017-12-22 22:20:06 +0800

    Update ex_09_04.py

--- ex_09_04.py
+++ ex_09_04.py
@@ -4,3 +4,28 @@ The program looks for 'From ' lines and takes the second word of those lines as 
 The program creates a Python dictionary that maps the sender's mail address to a count of the number of times they appear in the file. 
 After the dictionary is produced, the program reads through the dictionary using a maximum loop to find the most prolific committer.
 '''
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+names = list()
+counts = {}
+largestValue = None
+largestName = None
+
+for line in handle:
+    if not line.startswith("From:"):
+        continue
+    else:
+        names.append(line.split()[1])
+
+for name in names:
+    counts[name] = counts.get(name, 0) + 1
+
+for name,count in counts.items():
+    if largestValue is None or largestValue < count:
+        largestName = name
+        largestValue = count
+        
+print(largestName, largestValue)
