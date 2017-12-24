c7a4c3f90338ed6bcfc35d79aea3dd2de8ec1641
KelvinYu <ykq1312@126.com>
2017-12-24 20:01:05 +0800

    Create ex_11_01_using_sum_function.py

---
+++ ex_11_01_using_sum_function.py
@@ -0,0 +1,23 @@
+# using the fucntion sum() to get the sum of the list
+
+import re
+
+filename = input("File name:")
+if len(filename) < 1: filename = "regex_sum_55433.txt"
+
+nums = list()
+counts = dict()
+
+with open(filename) as f:
+	for line in f:
+		line = line.rstrip()
+		tmpnums = re.findall('[0-9]+', line)
+		for tmpnum in tmpnums:
+			nums.append(tmpnum)
+
+for i in range(len(nums)):
+	nums[i] = int(nums[i])
+
+tot = sum(nums)
+
+print(tot)
