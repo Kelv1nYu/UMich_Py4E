845fde4d0e1ecdf668e179ce44d2c1562902ff75
KelvinYu <ykq1312@126.com>
2018-01-08 21:28:21 +0800

    Create ex_13_02.py

---
+++ ex_13_02.py
@@ -0,0 +1,15 @@
+import json
+import urllib.request, urllib.parse, urllib.error
+
+url = 'http://py4e-data.dr-chuck.net/comments_55438.json'
+
+data = urllib.request.urlopen(url).read()
+
+info = json.loads(data)
+
+tot = 0
+for item in info['comments']:
+    #print(item['count'])
+    tot = tot + int(item['count'])
+
+print(tot)
