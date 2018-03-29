dfc4ee97d82184c190a732385e7c624512e1ce70
KelvinYu <ykq1312@126.com>
2018-03-29 20:20:47 +0800

    Update ex_02_02.py

--- ex_02_02.py
+++ ex_02_02.py
@@ -1,8 +1,4 @@
 # The code below almost works
-'''
-Write a program that uses input to prompt a user for their name and then welcomes them. 
-Note that input will pop up a dialog box. 
-Enter Sarah in the pop-up box when you are prompted so your output will match the desired output.
-'''
+# welcome people
 name = input("Enter your name")
 print("Hello", name)
