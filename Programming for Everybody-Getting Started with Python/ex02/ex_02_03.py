14ef7f2cc8f90a840071d1d0be106a44f3907755
KelvinYu <ykq1312@126.com>
2018-03-29 20:24:47 +0800

    Update and rename ex_02_03.py to Programming for Everybody-Getting Started with Python/ex02/ex_02_03.py

---
+++ Programming for Everybody-Getting Started with Python/ex02/ex_02_03.py
@@ -0,0 +1,7 @@
+# This first line is provided for you
+# prompt the user for hours and rate per hour using input to compute gross pay. 
+
+hrs = input("Enter Hours:")
+rate = input("Enter rate:")
+pay = float(hrs) * float(rate)
+print("Pay:", pay)
