1cb4ef07b23d96d31bf6af1df87bcd4f09907f9d
KelvinYu <ykq1312@126.com>
2018-03-29 20:29:16 +0800

    Update and rename ex_03_03.py to Programming for Everybody-Getting Started with Python/ex03/ex_03_03.py

--- Programming for Everybody-Getting Started with Python/ex03/ex_03_03.py
+++ Programming for Everybody-Getting Started with Python/ex03/ex_03_03.py
@@ -1,18 +1,12 @@
-"""Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. If the score is between 0.0 and 1.0, print a grade using the following table:
-Score Grade
->= 0.9 A
->= 0.8 B
->= 0.7 C
->= 0.6 D
-< 0.6 F
-If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.
-"""
+# prompt for a score between 0.0 and 1.0. 
+# input
 score = input("Enter Score: ")
+# check if folat
 try:
     fs = float(score)
 except:
     print("Error! Please input a number")
-
+# check the score
 if fs > 1.0 or fs < 0:
     print("Error! Please input a number between 0.0 and 1.0")
     quit()
